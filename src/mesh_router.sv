7174a89df8ba1aa30c7f2a65b11b4a78df0cac72
bag-wonho <olivyou2@gmail.com>
2026-08-31 17:44:00 +0900

    skid buffer implemented

--- src/mesh_router.sv
+++ src/mesh_router.sv
@@ -1,6 +1,39 @@
-module mesh_router(
+module mesh_router#(
+    parameter ADDR_WIDTH = 32,
+    parameter DATA_WIDTH = 64,
+    parameter CHANNELS = 2,
+
+    parameter X_BITS=2,
+    parameter Y_BITS=2,
+    parameter X=0,
+    parameter Y=0,
+
+    parameter FIFO_DEPTH=8
+)(
     input logic clk,
-    input logic rst_n
+    input logic rst_n,
+
+    input logic [ADDR_WIDTH-1: 0] north_addr_in[CHANNELS],
+    input logic [DATA_WIDTH-1: 0] north_data_in[CHANNELS],
+    input logic north_data_valid[CHANNELS],
+    output logic north_data_ready[CHANNELS],
+
+    input logic [ADDR_WIDTH-1: 0] west_addr_in[CHANNELS],
+    input logic [DATA_WIDTH-1: 0] west_data_in[CHANNELS],
+    input logic west_data_valid[CHANNELS],
+    output logic west_data_ready[CHANNELS],
+
+    output logic [ADDR_WIDTH-1: 0] east_addr_out[CHANNELS],
+    output logic [DATA_WIDTH-1: 0] east_data_out[CHANNELS],
+    output logic east_data_valid[CHANNELS],
+    input logic east_data_ready[CHANNELS],
+
+    output logic [ADDR_WIDTH-1: 0] south_addr_out[CHANNELS],
+    output logic [DATA_WIDTH-1: 0] south_data_out[CHANNELS],
+    output logic south_data_valid[CHANNELS],
+    input logic south_data_ready[CHANNELS]
 );
+
+    
 
 endmodule;
---
+++ src/utils/skid.sv
@@ -0,0 +1,33 @@
+module skid#(
+    parameter DATA_WIDTH=96
+)(
+    input logic clk,
+    input logic rst_n,
+
+    input logic [DATA_WIDTH-1: 0] input_data,
+    input logic input_valid,
+    output logic input_ready,
+
+    output logic [DATA_WIDTH-1: 0] output_data,
+    output logic output_valid,
+    input logic output_ready
+);
+
+    logic skid_valid;
+    logic [DATA_WIDTH-1:0] skid_data;
+
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            skid_valid <= 0;
+            skid_data <= 0;
+
+            input_ready <= 1;
+        end else begin
+            if (input_valid && input_ready) begin
+                
+            end
+        end
+    end
+
+endmodule
